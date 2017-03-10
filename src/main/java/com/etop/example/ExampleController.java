52a265f74970c2d62768f64449a0371ef369244a
jansing <lz_jansing@163.com>
2017-03-10 16:02:59 +0800

    版本二

--- src/main/java/com/etop/example/ExampleController.java
+++ src/main/java/com/etop/example/ExampleController.java
@@ -2,16 +2,24 @@ package com.etop.example;
 
 import com.etop.jansing.swopi.utils.SwopiUtil;
 import com.google.common.collect.Maps;
+import com.jansing.common.config.Global;
+import com.jansing.common.utils.Message;
+import com.jansing.web.utils.FileTransmitUtil;
 import com.jansing.web.utils.HttpClientUtil;
+import com.jansing.web.utils.StringUtil;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -84,5 +92,53 @@ public class ExampleController {
             params.put("os", SwopiUtil.OS_WIN);
         }
         return "redirect:" + convertServletPath + HttpClientUtil.encodeParams(params);
+    }
+
+
+    /**
+     * 单文件ajax上传
+     *
+     * @param req
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/uploadAjax", method = RequestMethod.POST)
+    public Message uploadAjax(@RequestParam CommonsMultipartFile file, HttpServletRequest req) throws IOException {
+        Message message = new Message();
+        try {
+            String filePath = HttpClientUtil.getLocalServerPath(req) + FileTransmitUtil.upload(file, req, Global.getConfig("uploadPath"));
+            message.setCode(Message.SUCCESS);
+            message.setMessage("上传成功");
+            message.getExtra().put(FileTransmitUtil.PATH_KEY, filePath);
+            //html的id，用以回显
+            String id = req.getParameter("id");
+            if (StringUtil.isNotBlank(id)) {
+                message.getExtra().put("id", id);
+            }
+        } catch (Exception e) {
+            message.setCode(Message.FAIL);
+            message.setMessage("上传失败，" + e.getMessage());
+        }
+        return message;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/askExist", method = {RequestMethod.GET, RequestMethod.POST})
+    public Message exist(@RequestParam String filePath, HttpServletRequest req) {
+        Message message = new Message();
+        if (StringUtil.isNotBlank(filePath)) {
+            if (new File(FileTransmitUtil.getRealPathFromUrl(req, filePath)).exists()) {
+                message.setCode(Message.SUCCESS);
+                message.setMessage("文件存在");
+            } else {
+                message.setCode(Message.FAIL);
+                message.setMessage("文件不存在");
+            }
+        } else {
+            message.setCode(Message.FAIL);
+            message.setMessage("参数不正确：filePath=" + filePath);
+        }
+        return message;
     }
 }
